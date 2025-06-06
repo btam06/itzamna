52a0427952168a072713b0eceb88ac1073d51505
Austin Fishbaugh <austinfishbaugh@imarc.com>
2025-06-06 14:51:29 -0400

    Allow adding translation rules to the translations array

--- src/Tools/TextFormatter.php
+++ src/Tools/TextFormatter.php
@@ -6,28 +6,45 @@ namespace Itzamna;
  */
 class TextFormatter
 {
+	protected array $translations = [
+		"</p>"   => '.\n  ',
+		"<br/>"  => '\n',
+		"\r\n"   => '\n',
+		"&nbsp;" => ' ',
+		","      => '\,',
+		";"      => '\;',
+		":"      => '\:'
+	];
+
+	/**
+	 * Add a translation to the formatter
+	 *
+	 * @param string $key The key to translate
+	 * @param string $value The value to translate to
+	 */
+	public function addTranslation(string $key, string $value) 
+	{
+		$this->translations[$key] = $value;
+	}
+
+	public function getTranslations() 
+	{
+		return $this->translations;
+	}
+
 	/**
 	 * Format HTML, strip tags, escape various characters for ICS text safety
 	 *
 	 * @param string $text The string to format
 	 * @return string Formatted text
 	 */
-	public function __invoke($text) {
-
-		$translations = array(
-			"</p>"   => '.\n  ',
-			"<br/>"  => '\n',
-			"\r\n"   => '\n',
-			"&nbsp;" => ' ',
-			","      => '\,',
-			";"      => '\;',
-			":"      => '\:'
-		);
-
+	public function __invoke($text) 
+	{
+		$text = str_replace(array_keys($this->translations), array_values($this->translations), $text);
 		$text = strip_tags($text, '<a>');
-		$text = str_replace(array_keys($translations), array_values($translations), $text);
 		$text = html_entity_decode($text);
 
 		return $text;
 	}
+
 }
